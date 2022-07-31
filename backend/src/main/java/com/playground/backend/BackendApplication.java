62077c059b1979958a9bc21ee466479fd7797980
ljb.laptop <laijianbin@163.com>
2022-07-31 23:08:01 +0800

    user login

--- backend/src/main/java/com/playground/backend/BackendApplication.java
+++ backend/src/main/java/com/playground/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.playerground.backend;
+package com.playground.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
