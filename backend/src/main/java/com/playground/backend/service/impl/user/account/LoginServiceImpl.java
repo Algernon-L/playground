d25c092f589597bfedaaf5f0ad5f31a968d355fa
ljb.laptop <laijianbin@163.com>
2022-08-01 22:58:12 +0800

    user login backend finish

---
+++ backend/src/main/java/com/playground/backend/service/impl/user/account/LoginServiceImpl.java
@@ -0,0 +1,41 @@
+package com.playground.backend.service.impl.user.account;
+
+import com.playground.backend.pojo.User;
+import com.playground.backend.service.impl.utils.UserDetailsImpl;
+import com.playground.backend.service.user.account.LoginService;
+import com.playground.backend.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public Map<String, String> getToken(String username, String password) {
+        // 生成加密后的token
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(username, password);
+        // 验证，异常自动处理
+        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+
+        UserDetailsImpl loginUser = (UserDetailsImpl) authenticate.getPrincipal();
+        User user = loginUser.getUser();
+
+        String jwt = JwtUtil.createJWT(user.getId().toString());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error message","Success");
+        map.put("token",jwt);
+
+        return map;
+    }
+}
--- web/src/App.vue
+++ web/src/App.vue
@@ -7,9 +7,57 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+import $ from 'jquery'
 export default{
   components:{
     NavBar
+  },
+  setup(){
+    $.ajax({
+      url: "http://127.0.0.1:3000/user/account/token/",
+      type: "post",
+      data:{
+        username: 'ljb',
+        password: 'pljb',
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    })
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/user/account/info/",
+      type: "get",
+      headers:{
+        Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkNzA4ZjI3ODkyZmQ0NmM3OGVjMDE2M2E0YmQ1MGNkMSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY1OTM2NDM5NSwiZXhwIjoxNjYwNTczOTk1fQ.Zqb1Kx_yVRWqhQ1zPgdPIEqXSvw9wUXekQmns8iNcVg"
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    })
+
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/user/account/register/",
+      type: "post",
+      data:{
+        username: "pxy",
+        password: "ppxy",
+        confirmedPassword: "ppxy"
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    })
   }
 }
 </script>
